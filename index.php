28bcf3cce6efe974b8825530bfdc34f8b9b11e7b
fullstack82 <fullstack8210@gmail.com>
2021-01-28 13:35:16 +0100

    else if anidados

--- index.php
+++ index.php
@@ -7,17 +7,50 @@ $ciudad = "Barcelona";
 $continente = "Asia";
 $mayoria_de_edad = 18;
 
-// Anidar if
-if($edad >= $mayoria_de_edad){
+// IF y anidar if
+if ($edad >= $mayoria_de_edad) {
     echo "<h1>$nombre es mayor de edad </h1>";
-    if($continente == "Europa"){
+    if ($continente == "Europa") {
         echo "<h2> y vive en $ciudad</h2>";
-    }else{
+    } else {
         echo "<h2> y No es europeo</h2>";
     }
-    
-}else{
+} else {
     echo "<h1>$nombre No es mayor de edad </h1>";
 };
 
 
+// Else If anidadps
+
+$dia = 4;
+
+if ($dia == 1) {
+    echo "Es lunes";
+} else {
+    if ($dia == 2) {
+        echo "Es martes";
+    } else {
+        if ($dia == 3) {
+            echo "Es miercoles";
+        } else {
+            if ($dia == 4){
+                echo "Es jueves";
+            }else{
+                if($dia == 5){
+                    echo "Es viernes";
+                }else{
+                    if($dia == 6){
+                        echo "Es sabado";
+                    }else{
+                        if($dia == 7){
+                            echo "Es domingo";
+                        }else{
+                            echo "No existen más días ";
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
